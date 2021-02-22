5b53b831c0c916283da9f0604d104a135c6b5866
brioche1703 <kevin.meniel@gmail.com>
2021-02-23 00:09:34 +0100

    Clean cached

---
+++ DirectXLearning/Keyboard.cpp
@@ -0,0 +1,88 @@
+#include "Keyboard.h"
+
+bool Keyboard::KeyIsPressed(unsigned char keycode) const noexcept {
+	return keyStates[keycode];
+}
+
+Keyboard::Event Keyboard::ReadKey() noexcept {
+	if (keyBuffer.size() > 0u) {
+		Keyboard::Event e = keyBuffer.front();
+		keyBuffer.pop();
+		return e;
+	}
+	else {
+		return Keyboard::Event();
+	}
+}
+
+bool Keyboard::KeyIsEmpty() const noexcept {
+	return keyBuffer.empty();
+}
+
+void Keyboard::FlushKey() noexcept {
+	keyBuffer = std::queue<Event>();
+}
+
+char Keyboard::ReadChar() noexcept {
+	if (charBuffer.size() > 0u) {
+		unsigned char charCode = charBuffer.front();
+		keyBuffer.pop();
+		return charCode;
+	}
+	else {
+		return 0;
+	}
+}
+
+bool Keyboard::CharIsEmpty() const noexcept {
+	return charBuffer.empty();
+}
+
+void Keyboard::FlushChar() noexcept {
+	charBuffer = std::queue<char>();
+}
+
+void Keyboard::Flush() noexcept {
+	FlushKey();
+	FlushChar();
+}
+
+void Keyboard::EnableAutorepeat() noexcept {
+	autorepeatEnabled = true;
+}
+
+void Keyboard::DisableAutorepeat() noexcept {
+	autorepeatEnabled = false;
+}
+
+bool Keyboard::AutorepeatIsEnabled() noexcept {
+	return autorepeatEnabled;
+}
+
+void Keyboard::OnKeyPressed(unsigned char keycode) noexcept {
+	keyStates[keycode] = true;
+	keyBuffer.push(Keyboard::Event(Keyboard::Event::Type::Press, keycode));
+	TrimBuffer(keyBuffer);
+}
+
+void Keyboard::OnKeyReleased(unsigned char keycode) noexcept {
+	keyStates[keycode] = false;
+	keyBuffer.push(Keyboard::Event(Keyboard::Event::Type::Release, keycode));
+	TrimBuffer(keyBuffer);
+}
+
+void Keyboard::OnChar(char character) noexcept {
+	charBuffer.push(character);
+	TrimBuffer(charBuffer);
+}
+
+void Keyboard::ClearState() noexcept {
+	keyStates.reset();
+}
+
+template<typename T>
+static void Keyboard::TrimBuffer(std::queue<T>& buffer) noexcept {
+	while (buffer.size() > bufferSize) {
+		buffer.pop();
+	}
+}
---
+++ DirectXLearning/Window.cpp
@@ -0,0 +1,166 @@
+#include "Window.h"
+
+#include <sstream>
+#include "resource.h"
+
+Window::WindowClass Window::WindowClass::wndClass;
+
+Window::WindowClass::WindowClass() noexcept
+	: hInst(GetModuleHandle(nullptr)) {
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = HandleMsgSetup;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = GetInstance();
+	wc.hIcon = static_cast<HICON>(LoadImage(GetInstance(), MAKEINTRESOURCE(IDI_ICON1), IMAGE_ICON, 32, 32, 0));
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = GetName();
+	wc.hIconSm = static_cast<HICON>(LoadImage(GetInstance(), MAKEINTRESOURCE(IDI_ICON1), IMAGE_ICON, 16, 16, 0));
+
+	RegisterClassEx(&wc);
+}
+
+Window::WindowClass::~WindowClass() {
+	UnregisterClass(wndClassName, GetInstance());
+}
+
+const char* Window::WindowClass::GetName() noexcept {
+	return wndClassName;
+}
+
+HINSTANCE Window::WindowClass::GetInstance() noexcept {
+	return wndClass.hInst;
+}
+
+Window::Window(int width, int height, const char* name) {
+	RECT wr;
+	wr.left = 100;
+	wr.right = width + wr.left;
+	wr.top = 100;
+	wr.bottom = height + wr.top;
+
+	if (FAILED(AdjustWindowRect(&wr, WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU, FALSE))) {
+		throw DXWND_LAST_EXCEPT();
+	}
+
+	HWND hWnd = CreateWindow(
+		WindowClass::GetName(),
+		name,
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		wr.right - wr.left,
+		wr.bottom - wr.top,
+		nullptr,
+		nullptr,
+		WindowClass::GetInstance(),
+		this);
+
+	if (hWnd == nullptr) {
+		throw DXWND_LAST_EXCEPT();
+	}
+
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
+}
+
+Window::~Window() {
+	DestroyWindow(hWnd);
+}
+
+LRESULT WINAPI Window::HandleMsgSetup(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept{
+	if (msg == WM_NCCREATE) {
+		const CREATESTRUCTW* const pCreate = reinterpret_cast<CREATESTRUCTW*>(lParam);
+		Window* const pWnd = static_cast<Window*>(pCreate->lpCreateParams);
+
+		SetWindowLongPtr(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(pWnd));
+		SetWindowLongPtr(hWnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(&Window::HandleMsgThunk));
+
+		return pWnd->HandleMsg(hWnd, msg, wParam, lParam);
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+LRESULT WINAPI Window::HandleMsgThunk(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept {
+	Window* const pWnd = reinterpret_cast<Window*>(GetWindowLongPtr(hWnd, GWLP_USERDATA));
+	return pWnd->HandleMsg(hWnd, msg, wParam, lParam);
+}
+
+LRESULT Window::HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept {
+	switch (msg) {
+	case WM_CLOSE:
+		PostQuitMessage(0);
+		return 0;
+	case WM_KILLFOCUS :
+		kbd.ClearState();
+		break;
+	case WM_KEYDOWN:
+	case WM_SYSKEYDOWN:
+
+		if (!(lParam & 0x40000000) || kbd.AutorepeatIsEnabled()) {
+			kbd.OnKeyPressed(static_cast<unsigned char>(wParam));
+		}
+		break;
+	case WM_KEYUP:
+	case WM_SYSKEYUP:
+		kbd.OnKeyReleased(static_cast<unsigned char>(wParam));
+		break;
+	case WM_CHAR:
+		kbd.OnChar(static_cast<unsigned char>(wParam));
+		break;
+	}
+
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+Window::Exception::Exception(int line, const char* file, HRESULT hr) noexcept
+	:
+	DirectXException(line, file),
+	hr(hr) {}
+
+const char* Window::Exception::what() const noexcept {
+	std::ostringstream oss;
+	oss << GetType() << std::endl
+		<< "[Error Code] " << GetErrorCode() << std::endl
+		<< "[Description] " << GetErrorString() << std::endl
+		<< GetOriginString();
+	whatBuffer = oss.str();
+	return whatBuffer.c_str();
+}
+
+const char* Window::Exception::GetType() const noexcept {
+	return "DirectX Window Exception";
+}
+
+std::string Window::Exception::TranslateErrorCode(HRESULT hr) noexcept {
+	char* pMsgBuf = nullptr;
+	DWORD nMsgLen = FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER
+		| FORMAT_MESSAGE_FROM_SYSTEM
+		| FORMAT_MESSAGE_IGNORE_INSERTS,
+		nullptr,
+		hr,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		reinterpret_cast<LPSTR>(&pMsgBuf),
+		0,
+		nullptr);
+
+	if (nMsgLen == 0) {
+		return "Unindentified error code";
+	}
+
+	std::string errorString = pMsgBuf;
+	LocalFree(pMsgBuf);
+	return errorString;
+}
+
+HRESULT Window::Exception::GetErrorCode() const noexcept {
+	return hr;
+}
+
+std::string Window::Exception::GetErrorString() const noexcept {
+	return TranslateErrorCode(hr);
+}
