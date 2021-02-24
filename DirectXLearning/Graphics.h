021e1804644f487a0566655bc76d6f1d6f37075b
brioche1703 <kevin.meniel@gmail.com>
2021-02-24 17:16:39 +0100

    Add debug layer

--- DirectXLearning/Graphics.h
+++ DirectXLearning/Graphics.h
@@ -1,24 +1,59 @@
 #pragma once
 
 #include "IncludeWin.h"
+#include "DirectXException.h"
+#include "DxgiInfoManager.h"
+
 #include <d3d11.h>
+#include <wrl.h>
 
 class Graphics {
+public:
+	class Exception : public DirectXException {
+		using DirectXException::DirectXException;
+	};
+	
+	class HrException : public Exception {
+	public:
+		HrException(int line, const char* file, HRESULT hr, std::vector<std::string> infoMsgs = {}) noexcept;
+		const char* what() const noexcept override;
+		const char* GetType() const noexcept override;
+		HRESULT GetErrorCode() const noexcept;
+		std::string GetErrorString() const noexcept;
+		std::string GetErrorDescription() const noexcept;
+		std::string GetErrorInfo() const noexcept;
+
+	private:
+		HRESULT hr;
+		std::string info;
+	};
+
+	class DeviceRemovedException : public HrException {
+		using HrException::HrException;
+
+	public:
+		const char* GetType() const noexcept override;
+
+	private:
+		std::string reason;
+	};
+
 public:
 	Graphics(HWND hWnd);
 	Graphics(const Graphics&) = delete;
 	Graphics& operator=(const Graphics&) = delete;
-	~Graphics();
-	
+	~Graphics() = default;
+
 	void EndFrame();
-	void ClearBuffer(float red, float green, float blue) noexcept {
-		const float color[] = { red, green, blue };
-		pContext->ClearRenderTargetView(pTarget, color);
-	}
+	void ClearBuffer(float red, float green, float blue) noexcept;
 
 private:
-	ID3D11Device* pDevice = nullptr;
-	IDXGISwapChain* pSwapChain = nullptr;
-	ID3D11DeviceContext* pContext = nullptr;
-	ID3D11RenderTargetView *pTarget = nullptr;
+#ifndef NDEBUG
+	DxgiInfoManager infoManager;
+#endif // NDEBUG
+
+	Microsoft::WRL::ComPtr<ID3D11Device> pDevice;
+	Microsoft::WRL::ComPtr<IDXGISwapChain> pSwapChain;
+	Microsoft::WRL::ComPtr<ID3D11DeviceContext> pContext;
+	Microsoft::WRL::ComPtr<ID3D11RenderTargetView> pTarget;
 };
